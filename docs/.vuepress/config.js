c0277544cf35e0b5a2e84cd5d65b9d6aa786d17f
Adam Carr <adamcarr1976@gmail.com>
2019-08-14 21:35:08 +0100

    Add vuepress files

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: "Adam's Blog ",
+    description: "Musing's of a another random developer.",
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/'},
+            { text: 'GitHub', link: 'http://github.com/adamcarr1976' }
+        ]
+
+    } 
+}
+
